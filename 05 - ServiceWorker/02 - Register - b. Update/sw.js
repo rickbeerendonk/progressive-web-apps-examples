b8bc4ae53295d251c95613a5b456b1a80a2d45ac
Rick Beerendonk <rick@beerendonk.com>
2018-03-18 19:56:47 -0500

    Add ServiceWorker - Register - Update example.

---
+++ 05 - ServiceWorker/02 - Register - b. Update/sw.js
@@ -0,0 +1,9 @@
+// Mozilla Public License Version 2.0
+// Copyright © 2018 Rick Beerendonk
+
+// Instructions:
+// 1. Run the website once
+// 2. Uncomment the last line in the service worker file.
+// 3. Run the website again and observe the update.
+
+// console.log('-- Service Worker updated file --');
