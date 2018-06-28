b56d3b5ffbcecaf2b5969aa61c67e5ca2ab1f12d
Rick Beerendonk <rick@beerendonk.com>
2018-06-28 04:34:39 +0200

    Add "ServiceWorker - Register - Force Update" example.

---
+++ 02 - ServiceWorker/02 - Register - c. Force Update/sw.js
@@ -0,0 +1,21 @@
+// Mozilla Public License Version 2.0
+// Copyright © 2018 Rick Beerendonk
+
+// Instructions:
+// 1. Run the website once
+// 2. Uncomment the last line in the service worker file.
+// 3. Run the website again and observe the update.
+
+ console.log('-- Service Worker updated file --');
+
+self.addEventListener('install', event => {
+  console.log('[install] Skip waiting for the new ServiceWorker.');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[activate] Activating ServiceWorker.');
+ 
+  console.log('[activate] Claiming this ServiceWorker.');
+  event.waitUntil(self.clients.claim());
+});
