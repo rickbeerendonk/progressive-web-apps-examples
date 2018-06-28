22042806fff09c7cb568f4804279bab04f37f831
Rick Beerendonk <rick@beerendonk.com>
2018-06-28 05:36:24 +0200

    Add "Fetch - In ServiceWorker" example.

---
+++ 04 - Fetch/03 - In ServiceWorker/sw.js
@@ -0,0 +1,16 @@
+// Mozilla Public License Version 2.0
+// Copyright © 2018 Rick Beerendonk
+
+self.addEventListener('fetch', function(event) {
+  console.log('[fetch] Handling ', event.request.url);
+
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+        console.log('[fetch] Response:', response);
+        return response;
+      }).catch(function(error) {
+        console.error('[fetch] Failed:', error);
+        throw error;
+      })
+  );
+});
