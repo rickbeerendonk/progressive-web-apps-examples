5fc754fec0e14986b532462c8faa4e38830dcec0
Rick Beerendonk <rick@beerendonk.com>
2018-06-29 07:46:08 +0200

    Add examples of using cache in a ServiceWorker.

---
+++ 08 - Cache Storage/c. In Serviceworker/01 - Initial/sw.js
@@ -0,0 +1,19 @@
+// Mozilla Public License Version 2.0
+// Copyright © 2018 Rick Beerendonk
+
+const cacheName = 'cache1-v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        '.',
+        'index.html',
+        'flowers.json',
+        'img/tulips.png'
+      ]);
+
+      // Remark: We don't use the cached responses, so offline nothing is visible!
+    })
+  );
+});
---
+++ 08 - Cache Storage/c. In Serviceworker/02 - Use Cached Values/sw.js
@@ -0,0 +1,32 @@
+// Mozilla Public License Version 2.0
+// Copyright © 2018 Rick Beerendonk
+
+const cacheName = 'cache2-v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        '.',
+        'index.html',
+        'flowers.json',
+        'img/tulips.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        console.log('Cache response:', response);
+        return response || fetch(event.request).then(function(response) {
+          return response;
+        });
+      });
+    })
+  );
+});
