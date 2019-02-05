d8000b1cf26f926434a988ca5de285dafdf265a5
Rick Beerendonk <rick@beerendonk.com>
2019-02-05 01:14:30 +0100

    Add "Console - Count" example.

---
+++ 13 - Console/02 - Count/console-count.js
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+console.count('label A');
+console.count('label B');
+
+console.count('label A');
+console.count('label B');
+
+console.countReset('label A');
+
+console.count('label A');
+console.count('label B');
