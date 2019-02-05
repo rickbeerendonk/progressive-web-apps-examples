612f77c8547bfb70dfd3d3686f16867c73123507
Rick Beerendonk <rick@beerendonk.com>
2019-02-05 01:23:47 +0100

    Add "Console - Table" example.

---
+++ 13 - Console/03 - Table/console-table.js
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+// Object
+console.table({ a: 1, b: 2 });
+console.table({
+  html: { version: '5.3', specifications: 'http://w3c.github.io/html/' },
+  css: {
+    version: 2018,
+    specifications: 'https://www.w3.org/Style/CSS/specs.en.html'
+  },
+  javascript: {
+    version: 2018,
+    specifications: 'https://tc39.github.io/ecma262/'
+  }
+});
+
+// Array
+console.table([111, 222, 333]);
+console.table([[111, 222, 333], ['AAA', 'BBB', 'CCC']]);
