d8fe68cb3f6d9a2c76eb3c6fcb3d694ba2b3169d
Rick Beerendonk <rick@beerendonk.com>
2019-02-09 14:12:07 -0500

    Add "Cache Storage - In ServiceWorker - Scope" example.

---
+++ 07 - Cache Storage/c. In Serviceworker/04 - Scope/sw.js
@@ -0,0 +1,39 @@
+// European Union Public License version 1.2
+// Copyright © 2018 Rick Beerendonk
+
+const cacheName = 'cache4-v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        '.',
+        'index.html',
+        'hyacinth.html',
+        'narcissus.html',
+        'tulips.html',
+        'img/hyacinth.png',
+        'img/narcissus.png',
+        'img/tulips.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        return (
+          response ||
+          fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+        );
+      });
+    })
+  );
+});
