68f8a94e699afd9b79ddbf601b346a4825a333c1
Rick Beerendonk <rick@beerendonk.com>
2019-02-09 17:07:21 +0100

    Add "Console - Message Types" example.

---
+++ 13 - Console/01 - Message Types/console-message-types.js
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+console.log('log');
+
+console.info('info');
+console.warn('warn');
+console.error('error');
