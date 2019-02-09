c2e37b207cf753f987f964294e72a3623114b2d5
Rick Beerendonk <rick@beerendonk.com>
2019-02-09 17:47:05 +0100

    Add "Console - Assert" example.

---
+++ 13 - Console/07 - Assert/index.js
@@ -0,0 +1,5 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+console.assert(1 + 2 === 3, '1 + 2 is not 3');
+console.assert(1 + 2 === 2, '1 + 2 is not 2');
